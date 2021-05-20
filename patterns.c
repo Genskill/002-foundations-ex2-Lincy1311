e12d9300adc8baa247f1942334b0e6882066cd8f
Lincy1311 <71397922+Lincy1311@users.noreply.github.com>
2021-05-20 16:38:17 +0530

    Add files via upload

---
+++ patterns.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<cs50.h>
+void main()
+{  int i,j
+   int p = get_int("Enter type of pattern (1 or 2) ");
+   if(p==1)
+   {
+      int n = get_int("Enter number of rows ");
+      for(i=n;i>0;i--)
+      {  for(j=0;j<i;j++)
+         {  
+          printf("#");
+         }
+      printf("\n");
+    }    
+    else
+    { 
+       int y = get_int("Enter number of rows ");
+       int x=1;
+       for(i=y;i>0;i--)
+       { 
+         for(j=0;j<i-1;j++)
+         {  
+            printf(" ");
+       }
+       for(k=0;k<x;k++)
+       {  
+           printf("#");
+       }
+       printf("\n");
+       x++;
+    }
+}   
